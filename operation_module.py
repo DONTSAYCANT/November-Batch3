10cd4c835a498569c5862b4f36c7d78a665db19f
PARTHIPAN KANNAN <66922169+DONTSAYCANT@users.noreply.github.com>
2021-11-11 18:26:26 +0530

    Add files via upload

---
+++ operation_module.py
@@ -0,0 +1,12 @@
+def operation1(var1, var2):
+    print("operation 1")
+    return var1[0] + var1[-1] + var2[0] + var2[-1]
+
+
+def operation2(var1, var2):
+    print("operation 2")
+    return var1 ** var2
+
+def operation3(var1, var2):
+    print("operation 3")
+    return var1+var2
